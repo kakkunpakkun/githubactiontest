7da31a1202614ab4da9ec431584f6b688388b229
kakkunpakkun <kaku@c-fo.com>
2020-03-23 17:04:59 +0900

    check

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import {wait} from './wait'
 async function run(): Promise<void> {
   try {
     const ms: string = core.getInput('milliseconds')
+    const issue_assignee = core.getInput('issue_assignee').replace(/\s+/g, '').split(',')
+    core.info(issue_assignee)
     core.debug(`Waiting ${ms} milliseconds ...`)
 
     core.debug(new Date().toTimeString())
